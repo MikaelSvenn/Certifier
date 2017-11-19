3bb90b301db3ad77d6cc0a59a25fce1ede06dc75
Mikael Svenn <mikael.svenn@mixu.fi>
2017-11-19 15:47:03 +0200

    Updated console ui assembly information.

--- Ui.Console/Properties/AssemblyInfo.cs
+++ Ui.Console/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 
-[assembly: AssemblyTitle("Ui.Console")]
+[assembly: AssemblyTitle("Certifier Console")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Ui.Console")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyProduct("cc")]
+[assembly: AssemblyCopyright("Copyright © Mikael Svenn 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
